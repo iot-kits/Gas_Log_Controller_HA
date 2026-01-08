5c94b30bb853cd5840f1564e73666bba5dd5920e
Karl Berger <W4KRL@arrl.net>
2026-01-08 14:19:25 -0500

    Expand documentation

--- include/thermostat.h
+++ include/thermostat.h
@@ -1,5 +1,7 @@
 /**
  * @file thermostat.h
+ * @version 2026.01.08
+ * @author Karl Berger & MS Copilot
  * @brief Thermostat control interface for heating system management.
  * 
  * This header file provides the interface for thermostat functionality,
@@ -16,8 +18,8 @@
  * 
  * @param roomTemp The current room temperature in degrees (Celsius or Fahrenheit).
  * @param setpoint The desired target temperature in degrees (same unit as roomTemp).
- * @return true if heating should be activated (roomTemp < setpoint).
- * @return false if heating should remain off (roomTemp >= setpoint).
+ * @return true if heating should be activated (roomTemp < setpoint - THERMOSTAT_HYSTERESIS).
+ * @return false if heating should remain off (roomTemp >= setpoint + THERMOSTAT_HYSTERESIS).
  */
 bool thermostatHeatCall(float roomTemp, float setpoint);
 
